ea7e9c8108b2b0fc6331376e1b0fc8d3672ca0b2
Babu Suriyanarayanan <babu28537@gmail.com>
2020-03-05 11:04:46 -0500

    2nd cut

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ManufactureComponent } from './manufacture/manufacture.component';
+import { TransformerComponent } from './transformer/transformer.component';
+import { ParameterComponent } from './parameter/parameter.component';
+import { SnIndicatorComponent } from './sn-indicator/sn-indicator.component';
+import { SnIndicatorChildComponent } from './sn-indicator-child/sn-indicator-child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DashboardComponent
+    DashboardComponent,
+    ManufactureComponent,
+    TransformerComponent,
+    ParameterComponent,
+    SnIndicatorComponent,
+    SnIndicatorChildComponent
   ],
   imports: [
     BrowserModule,
